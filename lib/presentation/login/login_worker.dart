bff745ab56e3c194de340ea885c8f7a939fd6a44
Tri Andre Wicaksono <triandrewicaksono@gmail.com>
2024-02-22 01:50:23 +0700

    update

--- lib/presentation/login/login_worker.dart
+++ lib/presentation/login/login_worker.dart
@@ -1,10 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:indivara_boilerplate/domain/entity/idp_auth/member_login.dart';
 import 'package:indivara_boilerplate/presentation/login/bloc/login_extender.dart';
+import 'package:indivara_boilerplate/ui_kit/get_state/get_state.dart';
+import 'package:indivara_boilerplate/ui_kit/get_state/get_state_builder.dart';
 import 'package:indivara_boilerplate/util/case/case.dart';
 import 'package:indivara_boilerplate/util/extension/extension.dart';
 import 'package:indivara_boilerplate/util/methods/methods.dart';
-
 part 'login_page.dart';
 
 mixin class _Worker {
